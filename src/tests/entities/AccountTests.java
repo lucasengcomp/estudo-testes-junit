04320ea1f5b0c9029cebd1fe36a75c9af3a967f5
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2021-10-19 10:27:04 -0300

    Pattern Factory added

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -5,38 +5,47 @@ import org.junit.jupiter.api.Test;
 
 import com.application.entities.Account;
 
+import tests.factory.AccountFactory;
+
 public class AccountTests {
 
 	/*
-	 * ARRANGE 
-	 * Instanciar os objetos necessários
-	*/
-	
+	 * ARRANGE Instanciar os objetos necessários
+	 */
+
 	/*
-	 * ACTION 
-	 * Executar as ações necessárias
-	*/
-	
+	 * ACTION Executar as ações necessárias
+	 */
+
 	/*
-	 * ASSERT
-	 * Resultado esperado
-	*/
-	
+	 * ASSERT Resultado esperado
+	 */
+
 	@Test
 	public void depositShouldIncreaseBalanceWhenPositiveAmount() {
-		
+		// ARRANGE
 		double amount = 200.0;
 		double expectedValue = 196.0;
-		Account account = new Account(1L, 0.0);
-		
+		Account account = AccountFactory.createEmptyAccount();
+
+		// ACTION
 		account.deposit(amount);
-		
+
+		// ASSERT
 		Assertions.assertEquals(expectedValue, account.getBalance());
-				
-		//ARRANGE 
-		
-		//ACTION
-		
-		//ASSERT
+	}
+
+	@Test
+	public void depositShouldDoNothingWhenNegativeAmount() {
+		// ARRANGE
+		double expectedValue = 100.0;
+		Account account = new Account(1L, expectedValue);
+		double amount = -200.0;
+
+		// ACTION
+		account.deposit(amount);
+
+		// ASSERT
+		Assertions.assertEquals(expectedValue, account.getBalance());
 	}
 }
